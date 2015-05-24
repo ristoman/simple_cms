be6a2bdd3ce8fa7426e23e05a5c94bd364b571c7
ristoman <ristoman@bitbucket.org>
2015-05-24 17:15:32 +0200

    link tests

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -6,6 +6,16 @@ class DemoController < ApplicationController
   	end
 
   	def hello
+  		@id = params[:id]
+  		@page = params['page']
   	end 
+
+  	def other_hello
+  		render('hello')
+  	end
+
+  	def gazzetta
+  		redirect_to('http://gazzetta.it')
+  	end
 
 end
